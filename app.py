1dff1a91ffce80e5e9bcabe73d6b63bad020806d
Victor <sv487131@gmail.com>
2026-06-05 13:39:49 +0100

    Added menu and topic saving

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-print("=== Cyber Notes ===\n")
+while True:
+    print("\n=== Cyber Notes ===")
+    print("1. View Topics")
+    print("2. Add Topic")
+    print("3. Exit")
 
-with open("topics.txt", "r") as file:
-    topics = file.readlines()
+    choice = input("Choose an option: ")
 
-for number, topic in enumerate(topics, start=1):
-    print(f"{number}. {topic.strip()}")
+    if choice == "1":
+        try:
+            with open("topics.txt", "r") as file:
+                topics = file.readlines()
+
+            print("\nSaved Topics:")
+            for number, topic in enumerate(topics, start=1):
+                print(f"{number}. {topic.strip()}")
+
+        except FileNotFoundError:
+            print("No topics file found.")
+
+    elif choice == "2":
+        topic = input("Enter new topic: ")
+
+        with open("topics.txt", "a") as file:
+            file.write(topic + "\n")
+
+        print("Topic saved!")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option.")
